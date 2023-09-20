782e2a5729d14b953ca307236de385d3f84e58e5
Mantas <mantas.ziln@gmail.com>
2023-09-20 19:31:00 +0300

    Added a basic html front for adding flashcards

---
+++ WebApplikacija/WebApplikacija/Pages/AddFlashcard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplikacija.Pages
+{
+    public class AddFlashcardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
