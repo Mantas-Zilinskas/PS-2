4ac908b5d0564732bd0c39b7b72d49c913a625d5
Mantas <mantas.ziln@gmail.com>
2023-09-21 21:52:12 +0300

    basic button handling

--- WebApplikacija/WebApplikacija/Pages/AddFlashcard.cshtml.cs
+++ WebApplikacija/WebApplikacija/Pages/AddFlashcard.cshtml.cs
@@ -5,8 +5,29 @@ namespace WebApplikacija.Pages
 {
     public class AddFlashcardModel : PageModel
     {
+        public string successMessage = string.Empty;
+
         public void OnGet()
         {
+            successMessage = string.Empty;
+        }
+
+        public void OnPostSubmitNewFlashcard()
+        {
+            string? submitButtonValue = Request.Form["submitButton"];
+            string? questionValue = Request.Form["question"];
+            string? answerValue = Request.Form["answer"];
+
+            if (submitButtonValue == "submit")
+            {
+                Console.WriteLine(questionValue);
+                Console.WriteLine(answerValue);
+
+                successMessage = "A new flashcard was successfully created";
+            }
+
+
+
         }
     }
 }
