39aaf934d03c9fc276e655090c1f0073e340282f
BuildTools <unconfigured@null.spigotmc.org>
2023-10-02 10:11:32 +0300

    Sutvarkyti Failai

---
+++ WebAplicationTestMVCSOL/WebAplicationTestMVC/Models/ImportDB.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAplicationTestMVC.Pages
+{
+    public class ImportDB : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public ImportDB(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            
+        }
+    } 
+}
---
+++ WebAplicationTestMVCSOL/WebAplicationTestMVC/Views/Home/_ModalPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAplicationTestMVC.Pages
+{
+    public class _ModalPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
