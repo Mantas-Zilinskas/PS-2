350d97f035a402b666229c983a4c15eab7ee576f
Mantas <mantas.ziln@gmail.com>
2023-10-02 22:27:41 +0300

    added a way to display flashcards that are inside of sets

--- WebApplikacija/WebApplikacija/Pages/StudySet.cshtml.cs
+++ WebApplikacija/WebApplikacija/Pages/StudySet.cshtml.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplikacija.Models;
+using WebApplikacija.Controllers;
 
 namespace WebApplikacija.Pages
 {
     public class StudySetModel : PageModel
     {
+        private readonly IWebHostEnvironment _env;
+
+        public StudySetModel(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        public List<Flashcard>? flashcards;
         public void OnGet()
         {
+            string studySet = Request.Query["setName"];
+            string rootPath = _env.ContentRootPath;
+            string filePath = Path.Combine(rootPath, "Data", "study sets", studySet);
+            flashcards = ExcelController.getExcelData(filePath);
         }
     }
 }
