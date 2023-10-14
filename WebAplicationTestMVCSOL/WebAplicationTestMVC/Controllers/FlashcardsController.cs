37146381ed8a358269d0d82e1f3e6e102b08f04e
Aurimas Vilčiūnas <a.vilciunas@gmail.com>
2023-10-14 23:18:18 +0300

    Added Controller page and different pages for flashcards learning by user settings (for developing)

---
+++ WebAplicationTestMVCSOL/WebAplicationTestMVC/Controllers/FlashcardsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAplicationTestMVC.Controllers
+{
+    public class FlashcardsController : Controller
+    {
+        public IActionResult RandomizedAndSystemCheck()
+        {
+            return View();
+        }
+        public IActionResult RandomizedAndUserCheck()
+        {
+            return View();
+        }
+        public IActionResult SpacedRepetitionAndSystemCheck()
+        {
+            return View();
+        }
+        public IActionResult SpacedRepetitionAndUserCheck()
+        {
+            return View();
+        }
+    }
+}
