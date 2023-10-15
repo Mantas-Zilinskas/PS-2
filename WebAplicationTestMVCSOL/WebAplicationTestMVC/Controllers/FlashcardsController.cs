d52846dc027ddb26975070f863c0d585ac554597
Mantas <mantas.ziln@gmail.com>
2023-10-15 12:31:39 +0300

    added randomizedAndSystemCheck flashcard mode

--- WebAplicationTestMVCSOL/WebAplicationTestMVC/Controllers/FlashcardsController.cs
+++ WebAplicationTestMVCSOL/WebAplicationTestMVC/Controllers/FlashcardsController.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using WebAplicationTestMVC.Models;
+using WebAplicationTestMVC.Utilities;
 
 namespace WebAplicationTestMVC.Controllers
 {
     public class FlashcardsController : Controller
     {
-        public IActionResult RandomizedAndSystemCheck()
+        public IActionResult RandomizedAndSystemCheck(string param1)
         {
-            return View();
+            List<Flashcard> flashcards = ExcelHelper.getExcelData(@"Data/" + param1);
+            return View(flashcards);
         }
         public IActionResult RandomizedAndUserCheck()
         {
