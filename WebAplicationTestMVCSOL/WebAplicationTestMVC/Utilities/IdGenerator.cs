3bda7004931b6b68431f84cdeb228e79fe96b9f5
Aurimas Vilčiūnas <a.vilciunas@gmail.com>
2023-10-17 11:59:35 +0300

    Edited generic type

--- WebAplicationTestMVCSOL/WebAplicationTestMVC/Utilities/IdGenerator.cs
+++ WebAplicationTestMVCSOL/WebAplicationTestMVC/Utilities/IdGenerator.cs
@@ -1,13 +1,13 @@
 ﻿namespace WebAplicationTestMVC.Utilities
 {
-    public class IdGenerator
+    public class IdGenerator<T>
     {
-        public static string generateId(string seed1, string seed2)
+        public static string GenerateId(T seed1, T seed2)
         {
             Random random = new Random();
 
             string id = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-            id = id + " " + seed1.Length + "_" + seed2.Length + "_" + random.Next(1, 10000000);
+            id = id + " " + seed1.ToString() + "_" + seed2.ToString() + "_" + random.Next(1, 10000000);
 
             return id;
         }
