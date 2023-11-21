41b260d769ecc0d093bb44e06189204a2c01d959
BuildTools <unconfigured@null.spigotmc.org>
2023-11-21 02:59:58 +0200

    Tests Added

---
+++ WebAplicationTestMVCSOL/TestProject1/ColorManagerTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using WebAplicationTestMVC.Models;
+using WebAplicationTestMVC.Utilities;
+
+namespace WebApplicationTestMVCTests
+{
+    [TestClass]
+    public class ColorManagerTests
+    {
+        private List<StudySet> studySets;
+        private Mock<HttpContext> mockHttpContext;
+        private Mock<HttpRequest> mockRequest;
+        private Mock<HttpResponse> mockResponse;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            studySets = new List<StudySet>
+            {
+                new StudySet("Mathematics"),
+                new StudySet("Science")
+              
+            };
+
+            mockHttpContext = new Mock<HttpContext>();
+            mockRequest = new Mock<HttpRequest>();
+            mockResponse = new Mock<HttpResponse>();
+            mockHttpContext.Setup(c => c.Request).Returns(mockRequest.Object);
+            mockHttpContext.Setup(c => c.Response).Returns(mockResponse.Object);
+
+            var cookieCollection = new Mock<IRequestCookieCollection>();
+            mockRequest.Setup(c => c.Cookies).Returns(cookieCollection.Object);
+        }
+
+        [TestMethod]
+        public void AssignUniqueColor_AssignsColorsAndSetsCookies()
+        {
+           
+            mockResponse.Setup(r => r.Cookies.Append(It.IsAny<string>(), It.IsAny<string>()));
+
+            
+            ColorManager.AssignUniqueColor(studySets, mockHttpContext.Object);
+
+           
+            foreach (var studySet in studySets)
+            {
+                Assert.IsTrue(Enum.IsDefined(typeof(StudySetColor), studySet.Color));
+            }
+            mockResponse.Verify(r => r.Cookies.Append(It.IsAny<string>(), It.IsAny<string>()), Times.AtLeastOnce());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CustomCookieNameException))]
+        public void AssignUniqueColor_ThrowsCustomCookieNameExceptionForInvalidStudySetName()
+        {
+           
+            studySets.Add(new StudySet("<InvalidName>")); 
+
+            
+            mockRequest.Setup(r => r.Cookies).Throws(new Exception("Simulated cookie access exception"));
+
+            
+            ColorManager.AssignUniqueColor(studySets, mockHttpContext.Object);
+
+            
+        }
+
+       
+    }
+}
---
+++ WebAplicationTestMVCSOL/TestProject1/StudySetServiceTest.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebAplicationTestMVC.Interface;
+using WebAplicationTestMVC.Models;
+using WebAplicationTestMVC.Services;
+
+namespace WebApplicationTestMVCTests
+{
+    [TestClass]
+    public class StudySetServiceTests
+    {
+        private Mock<IStudySetRepository> mockRepository;
+        private StudySetService service;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            mockRepository = new Mock<IStudySetRepository>();
+            service = new StudySetService(null, mockRepository.Object);
+        }
+
+        [TestMethod]
+        public void GetAllStudySets_ReturnsAllStudySets()
+        {
+            // Create mock StudySet objects with test data
+            var expectedStudySets = new List<StudySet>
+            {
+                new StudySet("Mathematics") { DateCreated = DateTime.Now.AddDays(-10), Id = 1 },
+                new StudySet("Science") { DateCreated = DateTime.Now.AddDays(-5), Id = 2 },
+                new StudySet("History") { DateCreated = DateTime.Now, Id = 3 }
+               
+            };
+
+           
+            mockRepository.Setup(repo => repo.GetAll()).Returns(expectedStudySets);
+
+          
+            var result = service.GetAllStudySets();
+
+           
+            Assert.AreEqual(expectedStudySets, result);
+        }
+
+        [TestMethod]
+        public void AddNewStudySet_AddsStudySetWhenNotExisting()
+        {
+           
+            var studySetName = "New Study Set";
+            mockRepository.Setup(repo => repo.GetByName(studySetName)).Returns((StudySet)null);
+
+            
+            service.AddNewStudySet(studySetName);
+
+          
+            mockRepository.Verify(repo => repo.Add(It.Is<StudySet>(s => s.StudySetName == studySetName)), Times.Once);
+        }
+
+      
+
+    }
+}
